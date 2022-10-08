ec7189233a5bda87841752f12440ec875ac0e871
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-08 16:54:41 +0600

    add cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = ({ cart }) => {
+    console.log(cart)
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Selected Item: {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
